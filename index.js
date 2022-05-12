e66711296bc7106d8c193e84b8847118a4781beb
buiquanghuy23103 <huythdnbkltv@gmail.com>
2022-05-12 16:06:16 +0300

    exercise 3.2

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const notes = [
 		"name": "Dan Abramov", 
 		"number": "12-43-234345"
 	},
-	{ 
+	{
 		"id": 4,
 		"name": "Mary Poppendieck", 
 		"number": "39-23-6423122"
@@ -26,6 +26,13 @@ const notes = [
 
 app.get('/api/persons', (request, response) => {
 	response.json(notes)
+})
+
+app.get('/info', (request, response) => {
+	const count = notes ? notes.length : 0
+	const date = new Date()
+	const info = `Phonebook has info for ${count}\n${date}`
+	response.end(info)
 })
 
 const PORT = 3001
