3420330a35225e1e3655e3e00f3f158be9ec4b8b
buiquanghuy23103 <huythdnbkltv@gmail.com>
2022-05-15 10:16:36 +0300

    add middleware

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 require('dotenv').config()
+
 const express = require('express')
 const app = express()
-app.use(express.json())
 
 const morgan = require('morgan')
 morgan.token('req-body', (request, _) => JSON.stringify(request.body))
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req-body'))
 
 const cors = require('cors')
-app.use(cors())
 
+app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req-body'))
+app.use(cors())
 app.use(express.static('build'))
 
 const Person = require('./models/person')
@@ -27,12 +28,15 @@ app.get('/info', (_, response) => {
 	return response.end(info)
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
 	const requestId = request.params.id
 	return Person.findById(requestId).then(person => {
 		if (!person)
 			return response.status(404).end()
 		return response.json(person)
+	}).catch(error => {
+		console.log('error=', error)
+		next(error)
 	})
 })
 
@@ -66,6 +70,14 @@ app.put('/api/persons/:id', (request, response) => {
 			return response.json(updatedPerson)
 	})
 })
+
+// errorMiddleware should always be at the end of this file
+const errorMiddleware = (error, request, response, next) => {
+	if (error.name === 'CastError')
+		return response.status(400).json({ error: 'malformed id' })
+	next(error)
+}
+app.use(errorMiddleware)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
